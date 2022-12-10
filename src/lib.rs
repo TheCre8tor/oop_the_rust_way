4cd5ed9cc83c288577d550314e28d9ee090b429e
TheCre8tor <cre8tor.alexander@gmail.com>
2022-12-10 22:19:03 +0100

    Treated oop encapsution priciple

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+pub struct AverageCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+/* EXPLANATION:
+ * If encapsulation is a required aspect for a language to
+ * be considered object-oriented, then Rust meets that requirement.
+ * The option to use pub or not for different parts of code enables
+ * encapsulation of implementation details.
+ * */
+
+impl AverageCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total = self.list.iter().sum::<i32>();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use oop_the_rust_way;
+
 fn main() {
-    println!("Hello, world!");
+    /* Rust is object-oriented: structs and enums have data,
+     * and impl blocks provide methods on structs and enums.
+     * Even though structs and enums with methods aren’t called
+     * objects, they provide the same functionality, according
+     * to the Gang of Four’s definition of objects.
+     * */
 }
